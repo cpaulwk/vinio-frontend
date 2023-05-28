869715c31655a7f057baaa75347b0b5efac1903c
Paul Chow <cpaulwenkai@gmail.com>
2023-05-28 17:40:17 +0200

    create custom tailwind classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,19 +11,11 @@ module.exports = {
       "brand-blue": "#042554",
       "brand-white": "#f8fdfd",
     },
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-        "hero-background": "url('public/images/hero-background.png')",
-      },
-    },
     fontSize: {
       s: "0.8125rem",
       base: "1rem",
       l: "1.25rem",
-      xl: "1.625",
+      xl: "1.625rem",
       "2xl": "2.0625rem",
       "3xl": "2.625rem",
     },
