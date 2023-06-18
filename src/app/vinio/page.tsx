688df286d3f1a20ed78a848a9aaefbf1cc472d66
Paul Chow <cpaulwenkai@gmail.com>
2023-06-18 20:26:51 +0200

    update vinio web app with basic features

--- src/app/vinio/page.tsx
+++ src/app/vinio/page.tsx
@@ -1,62 +1,125 @@
 "use client";
 
-import Header from "../../components/organisms/Header";
-import VinioTable from "../../components/organisms/VinioTable";
-import SubmitButton from "../../components/atoms/SubmitButton";
-import { ChangeEvent, useState } from "react";
+import Header from "@/components/organisms/Header";
+import VinioTable from "@/components/organisms/VinioTable";
+import SubmitButton from "@/components/atoms/SubmitButton";
+import { useState } from "react";
+import Results from "@/components/organisms/Results";
+import ErrorMessage from "@/components/organisms/ErrorMessage";
+
+interface Product {
+  product_id: number;
+  product: string;
+  parent_id: number;
+}
+
+interface OtherSuggestions {
+  grape_variety: string;
+}
 
 export default function Vinio() {
-  const [leftInput, setLeftInput] = useState<string>("");
-  const [rightInput, setRightInput] = useState<string>("");
-
-  const handleInputChange = (e: ChangeEvent<HTMLInputElement>): void => {
-    if (e.target.name === "leftInput") {
-      setLeftInput(e.target.value);
-    } else if (e.target.name === "rightInput") {
-      setRightInput(e.target.value);
-    }
-  };
-
-  console.log("leftInput => ", leftInput);
-  console.log("rightInput => ", rightInput);
+  const [results, setResults] = useState<Product[] | null | string>(null);
+  const [otherSuggestions, setOtherSuggestions] = useState<
+    OtherSuggestions[] | null
+  >(null);
+  const [query, setQuery] = useState({ leftQuery: "", rightQuery: "" });
+  const [isSuggesting, setIsSuggesting] = useState(false);
 
   const getSuggestion = async (): Promise<void> => {
+    setIsSuggesting(true);
     const requestData = {
-      leftInput: leftInput,
+      grape_variety: query.leftQuery,
     };
 
     try {
-      const response = await fetch("http://localhost:3001/pairing/grape-variety", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(requestData),
-      });
-      const data = await response.json();
+      const response1 = await fetch(
+        "http://localhost:3001/pairing/grape-variety",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(requestData),
+        }
+      );
+      const data1 = await response1.json();
+      setResults(data1.pairing);
 
-      console.log("data => ", data.pairing);
+      const response2 = await fetch(
+        "http://localhost:3001/pairing/additional-suggestion",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(requestData),
+        }
+      );
+      const data2 = await response2.json();
+      setOtherSuggestions(data2.pairing);
+
     } catch (error) {
       console.error(error);
     }
   };
 
-  const pair = (): void => {
-    console.log("Clicked on Pair!");
-    if (!leftInput || !rightInput) {
-      console.log("Please fill both fields");
+  const pair = async (): Promise<void> => {
+    setIsSuggesting(false);
+    if (!query.leftQuery && !query.rightQuery) {
+      setResults([]);
       return;
+    }
+
+    if (!query.leftQuery || !query.rightQuery) {
+      setResults("Please fill both fields to pair");
+      return;
+    }
+
+    const requestData = {
+      grape_variety: query.leftQuery,
+      product: query.rightQuery,
+    };
+
+    try {
+      const response = await fetch(
+        "http://localhost:3001/pairing/pairing-result",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(requestData),
+        }
+      );
+      const data = await response.json();
+
+      if (data.pairing.length) {
+        if (data.pairing[0].excellent_pairing) {
+          setResults("It's an excellent pairing!");
+        } else {
+          setResults("It's a good match");
+        }
+      } else {
+        setResults("There are better pairing options");
+      }
+    } catch (error) {
+      console.error(error);
     }
   };
 
   return (
-    <main className="flex w-screen flex-col  items-center justify-between overflow-hidden bg-brand-white">
+    <main className="flex w-screen flex-col  items-center justify-between bg-brand-white">
       <div className="flex h-screen w-screen flex-col">
         <Header page="Vinio" />
         <div className="flex grow flex-col justify-center gap-y-[5.6875rem] p-[6.25rem]">
           <div className="flex grow flex-col items-center justify-center gap-y-[3.5rem]">
-            <VinioTable handleInputChange={handleInputChange} />
-            <div className="flex items-center justify-center gap-x-[2.75rem]">
+            <VinioTable setQuery={setQuery} query={query} />
+            {results?.length !== 0 ? (
+              <Results results={results} otherSuggestions={otherSuggestions} isSuggesting={isSuggesting} />
+            ) : (
+              <ErrorMessage />
+            )}
+            <div className="flex flex-col items-center justify-center gap-[2.75rem] sm:flex-row">
               <SubmitButton
                 name="Get Suggestion!"
                 color="blue"
--- src/components/organisms/VinioTable.tsx
+++ src/components/organisms/VinioTable.tsx
@@ -1,40 +1,281 @@
-import { ChangeEvent } from "react";
+import { useState, useEffect, useRef } from "react";
+import ToggleListModal from "@/components/molecules/ToggleListModal";
+import SearchInput from "@/components/molecules/SearchInput";
+import { fetchAutocompleteData } from "@/utils/dataAutocompletion";
 
 type VinioTableProps = {
-  handleInputChange: (value: ChangeEvent<HTMLInputElement>) => void;
+  query: Query;
+  setQuery: (input: Query) => void;
 };
 
-export default function VinioTable({ handleInputChange }: VinioTableProps) {
+interface AutocompleteOptions {
+  allOptions: Record<string, string[]>;
+  leftOptions: string[];
+  rightOptions: string[];
+}
+
+interface Query {
+  leftQuery: string;
+  rightQuery: string;
+}
+
+export default function VinioTable({ query, setQuery }: VinioTableProps) {
+  const ulRef = useRef<HTMLUListElement>(null);
+
+  const [toggleList, setToggleList] = useState<string | null>(null);
+  const [condition, setCondition] = useState<{
+    leftCondition: string;
+    rightCondition: string;
+  }>({
+    leftCondition: "Grape Variety",
+    rightCondition: "Cheese",
+  });
+  const [filteredSuggestions, setFilteredSuggestions] = useState<string[]>([]);
+  const [autocompleteOptions, setAutocompleteOptions] =
+    useState<AutocompleteOptions>({
+      allOptions: {},
+      leftOptions: [],
+      rightOptions: [],
+    });
+  const [isListOpen, setIsListOpen] = useState<{
+    leftSuggestion: boolean;
+    rightSuggestion: boolean;
+  }>({
+    leftSuggestion: false,
+    rightSuggestion: false,
+  });
+  // const [query, setQuery] = useState({ leftQuery: "", rightQuery: "" });
+  const [label, setLabel] = useState<{ leftLabel: string; rightLabel: string }>(
+    {
+      leftLabel: "grape variety",
+      rightLabel: "cheese",
+    }
+  );
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetchAutocompleteData();
+
+      setAutocompleteOptions({
+        allOptions: data,
+        leftOptions: data.grapeVariety,
+        rightOptions: data.cheese,
+      });
+    };
+
+    fetchData();
+
+    const handleClickOutside = (event: MouseEvent) => {
+      if (ulRef.current && !ulRef.current.contains(event.target as Node)) {
+        setIsListOpen({ leftSuggestion: false, rightSuggestion: false });
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  const handleSuggestionClick = (id: string, suggestion: string) => {
+    const updatedQuery =
+      id === "leftSuggestion"
+        ? { ...query, leftQuery: suggestion }
+        : { ...query, rightQuery: suggestion };
+
+    const updatedIsListOpen =
+      id === "leftSuggestion"
+        ? { ...isListOpen, leftSuggestion: false }
+        : { ...isListOpen, rightSuggestion: false };
+
+    setQuery(updatedQuery);
+    setIsListOpen(updatedIsListOpen);
+  };
+
+  const filterSuggestions = (inputValue: string, options: string[]) =>
+    options.filter((suggestion: string) =>
+      suggestion.toLowerCase().includes(inputValue.toLowerCase())
+    );
+
+  const handleSearchInputClick = (e: React.MouseEvent<HTMLInputElement>) => {
+    const target = e.target as HTMLInputElement;
+    const leftSearchId = `${leftLabel[0].toLowerCase() + leftLabel.slice(1).replace(/(\s+)/g, "")
+      }Search`;
+    const rightSearchId = `${rightLabel[0].toLowerCase() + rightLabel.slice(1).replace(/(\s+)/g, "")
+      }Search`;
+
+    if (target.id === leftSearchId) {
+      const filtered = filterSuggestions(
+        query.leftQuery,
+        autocompleteOptions.leftOptions
+      );
+      setFilteredSuggestions(filtered);
+      setIsListOpen({
+        leftSuggestion: true,
+        rightSuggestion: false,
+      });
+    } else if (target.id === rightSearchId) {
+      const filtered = filterSuggestions(
+        query.rightQuery,
+        autocompleteOptions.rightOptions
+      );
+      setFilteredSuggestions(filtered);
+      setIsListOpen({
+        leftSuggestion: false,
+        rightSuggestion: true,
+      });
+    }
+  };
+
+  const handleSearchInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const inputValue = e.target.value;
+    // const camelCasedLabel = label.leftLabel.replace(/(?:^\w|[A-Z]|\b\w)/g, (match, index) => {
+    //   return index === 0 ? match.toLowerCase() : match.toUpperCase();
+    // }).replace(/\s+/g, '');
+    const isLeftInput =
+      e.target.id ===
+      `${label.leftLabel[0].toLowerCase() +
+      label.leftLabel.slice(1).replace(/(\s+)/g, "")
+      }Search`;
+
+    const filtered = isLeftInput
+      ? filterSuggestions(inputValue, autocompleteOptions.leftOptions)
+      : filterSuggestions(inputValue, autocompleteOptions.rightOptions);
+
+    setFilteredSuggestions(filtered);
+
+    const updatedQuery = isLeftInput
+      ? { ...query, leftQuery: e.target.value }
+      : { ...query, rightQuery: e.target.value };
+
+    const updatedIsListOpen = isLeftInput
+      ? { ...isListOpen, leftSuggestion: true, rightSuggestion: false }
+      : { ...isListOpen, leftSuggestion: false, rightSuggestion: true };
+
+    setQuery(updatedQuery);
+    setIsListOpen(updatedIsListOpen);
+  };
+
+  const handleToggleClick = (field: string) => {
+    if (toggleList === field) {
+      setToggleList(null);
+    } else {
+      setToggleList(field);
+    }
+  };
+
+  const handleSelectionClick = (field: string) => {
+    // const chosenField = field[0].toLowerCase() + field.slice(1).replace(/(\s+)/g, "");
+    const chosenField = field
+      .replace(/(?:^\w|[A-Z]|\b\w)/g, (match, index) => {
+        return index === 0 ? match.toLowerCase() : match.toUpperCase();
+      })
+      .replace(/\s+/g, "");
+    const updatedCondition =
+      toggleList === "leftCondition"
+        ? { ...condition, leftCondition: field }
+        : { ...condition, rightCondition: field };
+
+    const updatedLabel =
+      toggleList === "leftCondition"
+        ? { ...label, leftLabel: field }
+        : { ...label, rightLabel: field };
+
+    const updatedOptions =
+      toggleList === "leftCondition"
+        ? {
+          ...autocompleteOptions,
+          leftOptions: autocompleteOptions.allOptions[chosenField],
+        }
+        : {
+          ...autocompleteOptions,
+          rightOptions: autocompleteOptions.allOptions[chosenField],
+        };
+
+    const updatedQuery =
+      toggleList === "leftCondition"
+        ? { ...query, leftQuery: "" }
+        : { ...query, rightQuery: "" };
+
+    setCondition(updatedCondition);
+    setToggleList(null);
+    setLabel(updatedLabel);
+    setAutocompleteOptions(updatedOptions);
+    setQuery(updatedQuery)
+  };
+
+  const { leftCondition, rightCondition } = condition;
+  const { leftLabel, rightLabel } = label;
+
   return (
-    <section className="flex w-full grow flex-col items-center rounded border">
-      <div className="flex h-[2.75rem] w-full border-b">
+    <section className="flex w-full flex-col items-center rounded border border-brand-blue bg-brand-blue">
+      <div className="flex h-[2.75rem] w-full">
         <div className="flex grow items-center justify-center">
-          <div className="justity-center flex flex-1 items-center gap-x-[0.75rem] px-[1.5rem]">
-            <p>Wine</p>
-            <button>⌄</button>
+          <div className="justity-center flex flex-1 items-center px-[1.5rem]">
+            <label
+              id="leftCondition"
+              className="flex items-center justify-between p-[0.5rem] text-brand-white hover:cursor-pointer"
+              onClick={(e) => handleToggleClick(e.currentTarget.id)}
+            >
+              <p className="text-brand-white">{leftCondition}</p>
+              <span className="ml-[0.75rem]">⌄</span>
+            </label>
           </div>
-          <div className="justity-center flex flex-1 items-center gap-x-[0.75rem] px-[1.5rem]">
-            <p>Cheese</p>
-            <button>⌄</button>
+          <div className="justity-center flex flex-1 items-center px-[1.5rem]">
+            <label
+              id="rightCondition"
+              className="flex items-center justify-between p-[0.5rem] text-brand-white hover:cursor-pointer"
+              onClick={(e) => handleToggleClick(e.currentTarget.id)}
+            >
+              <p className="text-brand-white">{rightCondition}</p>
+              <span className="ml-[0.75rem]">⌄</span>
+            </label>
           </div>
         </div>
       </div>
-      <div className="flex w-full grow">
+      <div className="flex w-full">
         <div className="flex grow justify-center">
-          <div className="flex flex-1 border-r">
-            <input
-              name="leftInput"
-              className="w-full px-[1.5rem] py-[0.75rem]"
-              type="text"
-              onChange={(e) => handleInputChange(e)}
+          <div className="relative flex flex-1 border-r border-brand-blue">
+            {toggleList === "leftCondition" && (
+              <ToggleListModal
+                selectedCondition={leftCondition}
+                onClick={handleSelectionClick}
+              />
+            )}
+            <SearchInput
+              id={leftLabel}
+              value={query.leftQuery}
+              label={leftLabel}
+              suggestions={filteredSuggestions}
+              isOpen={isListOpen.leftSuggestion}
+              onClick={handleSearchInputClick}
+              onChange={handleSearchInputChange}
+              onSuggestionClick={(suggestion) =>
+                handleSuggestionClick("leftSuggestion", suggestion)
+              }
+              ulRef={ulRef}
             />
           </div>
-          <div className="flex flex-1">
-            <input
-              name="rightInput"
-              className="w-full px-[1.5rem] py-[0.75rem]"
-              type="text"
-              onChange={(e) => handleInputChange(e)}
+          <div className="relative flex flex-1">
+            {toggleList === "rightCondition" && (
+              <ToggleListModal
+                selectedCondition={rightCondition}
+                onClick={handleSelectionClick}
+              />
+            )}
+            <SearchInput
+              id={rightLabel}
+              value={query.rightQuery}
+              label={rightLabel}
+              suggestions={filteredSuggestions}
+              isOpen={isListOpen.rightSuggestion}
+              onClick={handleSearchInputClick}
+              onChange={handleSearchInputChange}
+              onSuggestionClick={(suggestion) =>
+                handleSuggestionClick("rightSuggestion", suggestion)
+              }
+              ulRef={ulRef}
             />
           </div>
         </div>
