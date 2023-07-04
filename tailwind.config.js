56ca216fe116809f8dcd63e966db12a5c1cbed53
Paul Chow <cpaulwenkai@gmail.com>
2023-07-04 17:11:35 +0200

    update all colors hex code for results

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,20 @@ module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     colors: {
+      white: "#ffffff",
       "brand-red": "#d0122d",
       "brand-blue": "#042554",
       "brand-white": "#f8fdfd",
+      "delicate-and-nutty-cheese": "#FAED70",
+      "fresh-and-salted-cheese": "#FFB977",
+      "smelly-cheese": "#A55B6E",
+      "strong-and-firm-cheese": "#F9B2B2",
+      charcuterie: "#CBB5A2",
+      fish: "#7D5642",
+      mollusc: "#284B63",
+      "red-meat": "#AEB9E8",
+      shellfish: "#B2E6D4",
+      "white-meat": "#7A9E7E",
     },
     fontSize: {
       s: "0.8125rem",
@@ -26,6 +37,13 @@ module.exports = {
     extend: {
       maxHeight: {
         240: "15rem",
+      },
+      flexBasis: {
+        "1/7": "14.285714%",
+        "1/8": "12.5%",
+        "1/9": "11.111111%",
+        "1/10": "10%",
+        "1/11": "9.090909%",
       },
     },
   },
