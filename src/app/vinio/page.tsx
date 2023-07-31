66a17672a110f6b8f54fbdeddd57b612900a9cb2
Paul Chow <cpaulwenkai@gmail.com>
2023-07-31 11:42:24 +0200

    clean code

--- src/app/vinio/page.tsx
+++ src/app/vinio/page.tsx
@@ -17,9 +17,16 @@ interface OtherSuggestions {
   grape_variety: string;
 }
 
+const NEXT_PUBLIC_BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
+
+const API_URLS = {
+  getSuggestion: `${NEXT_PUBLIC_BACKEND_URL}/pairing/grape-variety`,
+  getAdditionalSuggestions: `${NEXT_PUBLIC_BACKEND_URL}/pairing/additional-suggestion`,
+  pair: `${NEXT_PUBLIC_BACKEND_URL}/pairing/pairing-result`,
+};
+
 export default function Vinio() {
-  const [results, setResults] = useState<Product[] | null | string>("");
-  const NEXT_PUBLIC_BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
+  const [results, setResults] = useState<Product[] | string>("");
 
   const [otherSuggestions, setOtherSuggestions] = useState<
     OtherSuggestions[] | null
@@ -28,6 +35,23 @@ export default function Vinio() {
   const [isSuggesting, setIsSuggesting] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
   const [errorMessage, setErrorMessage] = useState("");
+
+  const fetchData = async (url: string, requestData: any) => {
+    try {
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(requestData),
+      });
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error(error);
+      throw new Error("An error occurred during the API request");
+    }
+  };
 
   const getSuggestion = async (): Promise<void> => {
     if (!query.leftQuery) {
@@ -45,39 +69,27 @@ export default function Vinio() {
     };
 
     try {
-      const suggestions = await fetch(
-        `${NEXT_PUBLIC_BACKEND_URL}/pairing/grape-variety`,
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(requestData),
-        }
+      const suggestionsResult = await fetchData(
+        API_URLS.getSuggestion,
+        requestData
       );
-      const suggestionsResult = await suggestions.json();
       setResults(suggestionsResult.pairing);
 
       if (!suggestionsResult.pairing.length) {
         setErrorMessage("");
         setResults("");
         setIsLoading(false);
+        return;
       }
 
-      const additionalSuggestions = await fetch(
-        `${NEXT_PUBLIC_BACKEND_URL}/pairing/additional-suggestion`,
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(requestData),
-        }
+      const additionalSuggestionsResult = await fetchData(
+        API_URLS.getAdditionalSuggestions,
+        requestData
       );
-      const additionalSuggestionsResult = await additionalSuggestions.json();
       setOtherSuggestions(additionalSuggestionsResult.pairing);
     } catch (error) {
       console.error(error);
+      throw new Error("Failed to get suggestions. Please try again");
     }
     setIsLoading(false);
   };
@@ -99,29 +111,20 @@ export default function Vinio() {
     };
 
     try {
-      const response = await fetch(
-        `${NEXT_PUBLIC_BACKEND_URL}/pairing/pairing-result`,
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(requestData),
-        }
-      );
-      const data = await response.json();
+      const data = await fetchData(API_URLS.pair, requestData);
 
       if (data.pairing.length) {
-        if (data.pairing[0].excellent_pairing) {
-          setResults("It's an excellent pairing!");
-        } else {
-          setResults("It's a good match");
-        }
+        setResults(
+          data.pairing[0].excellent_pairing
+            ? "It's an excellent pairing!"
+            : "It's a good match"
+        );
       } else {
         setResults("There are better pairing options");
       }
     } catch (error) {
       console.error(error);
+      throw new Error("Failed to get pairing result. Please try again later");
     }
     setIsLoading(false);
   };
@@ -144,7 +147,7 @@ export default function Vinio() {
                 <ErrorMessage errorMessage={errorMessage} />
               )
             ) : (
-              <section className="h-min-[2.75rem] flex w-full flex-1 flex-col items-center justify-center border-brand-blue bg-brand-white text-brand-white sm:rounded"></section>
+              <section className="h-min-[2.75rem] flex w-full flex-1 flex-col border border-brand-white bg-brand-white sm:rounded"></section>
             )}
           </div>
           <div className="flex basis-1/5 flex-col items-center justify-center gap-[2.75rem] pb-[1rem] sm:flex-row">
