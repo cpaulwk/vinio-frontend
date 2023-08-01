2319cffff88aae852b0e8588a716f4b29d9ebbe1
Paul Chow <cpaulwenkai@gmail.com>
2023-08-01 11:57:13 +0200

    clean code

--- src/components/organisms/ErrorMessage.tsx
+++ src/components/organisms/ErrorMessage.tsx
@@ -3,13 +3,13 @@ type ErrorMessageProps = {
 };
 
 export default function ErrorMessage({ errorMessage }: ErrorMessageProps) {
-  if (!errorMessage) {
-    errorMessage = "Sorry we could not find what you were looking for";
-  }
+  const defaultErrorMessage = "Sorry we could not find what you were looking for";
+
+  const message = errorMessage || defaultErrorMessage;
 
   return (
     <section className="h-min-[2.75rem] flex w-full flex-1 flex-col items-center justify-center rounded border-brand-blue bg-brand-red px-[1.5rem] py-[0.75rem] text-brand-white">
-      <p className="h-[1.6875rem]">{errorMessage}</p>
+      <p className="h-[1.6875rem]">{message}</p>
     </section>
   );
 }
