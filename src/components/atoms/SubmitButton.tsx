5d2477ad680561e14092cdd1c46fb87817a697c8
Paul Chow <cpaulwenkai@gmail.com>
2023-08-01 11:37:38 +0200

    clean code

--- src/components/atoms/SubmitButton.tsx
+++ src/components/atoms/SubmitButton.tsx
@@ -4,18 +4,15 @@ type SubmitButtonProps = {
   handleClick?: () => void;
 };
 
-const DEFAULT_BUTTON_COLOR = "red";
-
 export default function SubmitButton({
   name,
-  color = DEFAULT_BUTTON_COLOR,
+  color = "red",
   handleClick,
 }: SubmitButtonProps) {
-  const buttonColorClass = `bg-brand-${color}`;
 
   return (
     <button
-      className={`flex h-[3.125rem] w-[14.375rem] items-center justify-center rounded-[50px] max-xs:hidden bg-brand-${buttonColorClass} text-brand-white`}
+      className={`flex h-[3.125rem] w-[14.375rem] items-center justify-center rounded-[50px] max-xs:hidden bg-brand-${color} text-brand-white`}
       onClick={handleClick}
     >
       <p>{name}</p>
