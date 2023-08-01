99aa7313eb2481bf7f8fe65a480e8fd11b0d7e7f
Paul Chow <cpaulwenkai@gmail.com>
2023-08-01 11:54:18 +0200

    clean code

--- src/components/molecules/ToggleListModal.tsx
+++ src/components/molecules/ToggleListModal.tsx
@@ -3,40 +3,40 @@ type ToggleListModalProps = {
   selectedCondition: string;
 };
 
+const conditionMappings = [
+  {
+    conditions: ["Appellation", "Grape Variety"],
+    buttons: [
+      { label: "Appellation", value: "Appellation" },
+      { label: "Grape Variety", value: "Grape Variety" },
+    ],
+  },
+  {
+    conditions: ["Cheese", "Meat"],
+    buttons: [
+      { label: "Cheese", value: "Cheese" },
+      { label: "Meat", value: "Meat" },
+    ],
+  },
+];
+
 export default function ToggleListModal({
   onClick,
   selectedCondition,
 }: ToggleListModalProps) {
-  const conditionMappings = [
-    {
-      conditions: ["Appellation", "Grape Variety"],
-      buttons: [
-        { label: "Appellation", value: "Appellation" },
-        { label: "Grape Variety", value: "Grape Variety" },
-      ],
-    },
-    {
-      conditions: ["Cheese", "Meat"],
-      buttons: [
-        { label: "Cheese", value: "Cheese" },
-        { label: "Meat", value: "Meat" },
-      ],
-    },
-  ];
-
   return (
     <ul className="absolute left-[-1px] top-10 z-10 flex w-full min-w-[125px] flex-col rounded-b border border-brand-blue bg-brand-blue px-[0.75rem] py-[0.5rem] text-brand-white">
       {conditionMappings.map((mapping) =>
         mapping.conditions.includes(selectedCondition)
           ? mapping.buttons.map((button) => (
-              <li
-                className="cursor-pointer"
-                key={button.value}
-                onClick={() => onClick(button.value)}
-              >
-                {button.label}
-              </li>
-            ))
+            <li
+              key={button.value}
+              className="cursor-pointer"
+              onClick={() => onClick(button.value)}
+            >
+              {button.label}
+            </li>
+          ))
           : null
       )}
     </ul>
