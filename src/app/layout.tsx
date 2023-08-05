9797c54d91af6000b6db639215e5c4f4d4ad3b78
Paul Chow <cpaulwenkai@gmail.com>
2023-08-05 11:52:00 +0200

    remove favicon from jsx, because not needed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,9 +21,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-      </head>
       <body className={comme.className}>{children}</body>
     </html>
   );
