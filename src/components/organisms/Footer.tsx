29ba8d56bcaa4894686626d7a681ed69af0796c0
Paul Chow <cpaulwenkai@gmail.com>
2023-08-27 15:09:43 +0200

    Footer: Create footer component

---
+++ src/components/organisms/Footer.tsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+  return (
+    < footer className="h-[5vh] flex justify-center items-center text-brand-blue" >
+      <p>Made with love by Paul Chow 💙</p>
+    </footer >
+  );
+}
+
