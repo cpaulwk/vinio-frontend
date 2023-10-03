527f678d9dccfe153069ba5f9e77c2c5b6e557cc
Paul Chow <cpaulwenkai@gmail.com>
2023-10-03 21:13:19 +0200

    Button: update style button border

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -8,7 +8,7 @@ export default function Button({ name }: ButtonProps) {
   return (
     <Link
       href="/vinio"
-      className={`flex h-[3.125rem] w-[14.375rem] items-center justify-center rounded-[50px] border border-brand-white bg-brand-red text-brand-white max-xs:hidden`}
+      className={`flex h-[3.125rem] w-[14.375rem] items-center justify-center rounded-[50px] border-[3px] border-brand-white bg-brand-red text-brand-white max-xs:hidden`}
     >
       <p>{name}</p>
     </Link>
